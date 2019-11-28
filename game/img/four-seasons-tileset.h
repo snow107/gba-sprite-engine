83b517d17948a2ce80a6927daab11c64153a4b4b
snow107 <michiel.decosemaker@telenet.be>
2019-11-28 21:07:59 +0100

    tilemap+tileset+pallete

---
+++ game/img/four-seasons-tileset.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(four_seasons_tileset)
+
+//======================================================================
+//
+//	four_seasons_tileset, 192x272@8, 
+//	+ palette 256 entries, not compressed
+//	+ 816 tiles not compressed
+//	Total size: 512 + 52224 = 52736
+//
+//	Time-stamp: 2019-11-28, 12:14:47
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_FOUR_SEASONS_TILESET_H
+#define GRIT_FOUR_SEASONS_TILESET_H
+
+#define four_seasons_tilesetTilesLen 52224
+extern const unsigned short four_seasons_tilesetTiles[26112];
+
+#define four_seasons_tilesetPalLen 512
+extern const unsigned short four_seasons_tilesetPal[256];
+
+#endif // GRIT_FOUR_SEASONS_TILESET_H
+
+//}}BLOCK(four_seasons_tileset)
---
+++ game/src/scene_1/tile_pal.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(blok2)
+
+//======================================================================
+//
+//	blok2, 32x16@8, 
+//	+ palette 256 entries, not compressed
+//	+ 8 tiles not compressed
+//	Total size: 512 + 512 = 1024
+//
+//	Time-stamp: 2019-11-28, 21:02:33
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BLOK2_H
+#define GRIT_BLOK2_H
+
+#define blok2TilesLen 512
+extern const unsigned short blok2Tiles[256];
+
+#define blok2PalLen 512
+extern const unsigned short blok2Pal[256];
+
+#endif // GRIT_BLOK2_H
+
+//}}BLOCK(blok2)
