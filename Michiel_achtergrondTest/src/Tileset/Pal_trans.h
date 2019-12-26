598bb2d22d9571fea98118d7aeebb7746f5b6ec4
snow107 <michiel.decosemaker@telenet.be>
2019-12-26 17:26:36 +0100

    tileset.png

---
+++ Michiel_achtergrondTest/src/Tileset/Pal_trans.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(Pal_trans)
+
+//======================================================================
+//
+//	Pal_trans, 64x40@8, 
+//	+ palette 256 entries, not compressed
+//	+ 40 tiles not compressed
+//	Total size: 512 + 2560 = 3072
+//
+//	Time-stamp: 2019-12-12, 12:21:36
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_PAL_TRANS_H
+#define GRIT_PAL_TRANS_H
+
+#define Pal_transTilesLen 2560
+extern const unsigned short Pal_transTiles[1280];
+
+#define Pal_transPalLen 512
+extern const unsigned short Pal_transPal[256];
+
+#endif // GRIT_PAL_TRANS_H
+
+//}}BLOCK(Pal_trans)
