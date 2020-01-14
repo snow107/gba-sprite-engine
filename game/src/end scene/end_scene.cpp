1b2851672f28d791f02c2e0241ab28df37597945
snow107 <michiel.decosemaker@telenet.be>
2020-01-14 22:57:53 +0100

    end scene

---
+++ game/src/end scene/end_scene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by michi on 14/01/2020.
+//
+
+#include "end_scene.h"
---
+++ game/src/end scene/end_scene.h
@@ -0,0 +1,14 @@
+//
+// Created by michi on 14/01/2020.
+//
+
+#ifndef GBA_SPRITE_ENGINE_PROJECT_END_SCENE_H
+#define GBA_SPRITE_ENGINE_PROJECT_END_SCENE_H
+
+
+class end_scene {
+
+};
+
+
+#endif //GBA_SPRITE_ENGINE_PROJECT_END_SCENE_H
