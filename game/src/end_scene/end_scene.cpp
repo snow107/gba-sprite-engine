2292b8e95ec0419c26317daffb3556a68674db1a
jelle <jelle.biesmans@student.uhasselt.be>
2020-01-15 16:42:49 +0100

    did some stuff for end screen

--- game/src/end_scene/end_scene.cpp
+++ game/src/end_scene/end_scene.cpp
@@ -3,3 +3,79 @@
 //
 
 #include "end_scene.h"
+//
+// Created by jelle on 2019/11/17.
+//
+
+
+#include <libgba-sprite-engine/sprites/sprite_builder.h>
+#include <libgba-sprite-engine/background/text_stream.h>
+#include <libgba-sprite-engine/gba/tonc_memdef.h>
+#include <libgba-sprite-engine/gba_engine.h>
+#include <libgba-sprite-engine/effects/fade_out_scene.h>
+#include "../Tileset/tilesSpel.h"
+#include "../scene_1/Main_background.h"
+#include "../scene_start/startSceneSprites.h"
+#include "../scene_1/scene_1.h"
+
+std::vector<Background *> end_scene::backgrounds() {
+    return {};
+}
+
+std::vector<Sprite *> end_scene::sprites() {
+    return {kurby.get(),sonic.get(),cicle.get(),star.get()};
+}
+
+void end_scene::load() {
+    engine->enableText();
+    foregroundPalette = std::unique_ptr<ForegroundPaletteManager>(new ForegroundPaletteManager(sharedPal, sizeof(sharedPal)));
+
+    TextStream::instance().setText("THE END", 3, 7);
+    TextStream::instance().setText("BY MICHIEL AND JELLE", 6, 2);
+
+    SpriteBuilder<Sprite> builder;
+
+    kurby = builder
+            .withData(kurbyTiles, sizeof(kurbyTiles))
+            .withSize(SIZE_32_32) //smaller kurby because memory issues? or me stupid
+            .withAnimated(12,10)
+            .withLocation(GBA_SCREEN_WIDTH-32, 0)
+            .buildPtr();
+
+    sonic = builder
+            .withData(sonicTiles, sizeof(sonicTiles))
+            .withSize(SIZE_16_32)
+            .withAnimated(16,10)
+            .withLocation(0, 0)
+            .buildPtr();
+
+    star = builder
+            .withData(starTiles, sizeof(starTiles))
+            .withSize(SIZE_16_16)
+            .withAnimated(1,100)
+            .withLocation(GBA_SCREEN_WIDTH/3, 70)
+            .buildPtr();
+
+    cicle = builder
+            .withData(cirkelTiles, sizeof(cirkelTiles))
+            .withSize(SIZE_16_16)
+            .withAnimated(1,100)
+            .withLocation((GBA_SCREEN_WIDTH*2)/3, 70)
+            .buildPtr();
+
+    engine->getTimer()->start();
+}
+
+void end_scene::tick(u16 keys) {
+    TextStream::instance().setText(engine->getTimer()->to_string(), 18, 1);
+
+    if(((keys & KEY_A) || (keys & KEY_B))) {
+        engine->getTimer()->toggle();
+    }
+
+    if(keys & KEY_START) {
+        if(!engine->isTransitioning()) {
+            engine->transitionIntoScene(new scene_1(engine), new FadeOutScene(2));
+        }
+    }
+}
--- game/src/end_scene/end_scene.h
+++ game/src/end_scene/end_scene.h
@@ -5,8 +5,23 @@
 #ifndef GBA_SPRITE_ENGINE_PROJECT_END_SCENE_H
 #define GBA_SPRITE_ENGINE_PROJECT_END_SCENE_H
 
+#include <libgba-sprite-engine/scene.h>
 
-class end_scene {
+class end_scene : public Scene {
+private:
+    std::unique_ptr<Sprite> kurby;
+    std::unique_ptr<Sprite> sonic;
+    std::unique_ptr<Sprite> star;
+    std::unique_ptr<Sprite> cicle;
+
+public:
+    std::vector<Sprite *> sprites() override;
+    std::vector<Background *> backgrounds() override;
+
+    end_scene(std::shared_ptr<GBAEngine> engine) : Scene(engine) {}
+
+    void load() override;
+    void tick(u16 keys) override;
 
 };
 
